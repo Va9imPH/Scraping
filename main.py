52e48ab7d20ccb2686a27becd651636cff0a53e8
Va9imPH <96561712+Va9imPH@users.noreply.github.com>
2022-03-24 21:04:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+response = requests.get(
+    url="https://voyagecosmetics.ru/catalog/novinki"
+)
+
+html = bs(response.content, "html.parser")
+items = html.select(".col-md-4 > .product-thumb > .product-inner")
+
+with open("new.txt", "a", encoding="utf-8") as file:
+    for el in items:
+        title = el.select(".product-title > a")
+        file.write(title[0].text + "\n")
+
+page = 2
+
+with open("new.txt", "a", encoding="utf-8")as file:
+    while True:
+        response = requests.get(
+                url="https://voyagecosmetics.ru/catalog/novinki" + f"?page={page}"
+            )
+
+        html = bs(response.content, "html.parser")
+        items = html.select(".col-md-4 > .product-thumb > .product-inner")
+
+        if len(items):
+            for el in items:
+                title = el.select(".product-title > a")
+                file.write(title[0].text + "\n")
+
+            page += 1
+        else:
+            break
