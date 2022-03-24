db79da7e2148e1fe734f50a14db23806a184d5c9
Va9imPH <96561712+Va9imPH@users.noreply.github.com>
2022-03-24 21:17:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ response = requests.get(
 html = bs(response.content, "html.parser")
 items = html.select(".col-md-4 > .product-thumb > .product-inner")
 
+# collecting data from the first page
 with open("new.txt", "a", encoding="utf-8") as file:
     for el in items:
         title = el.select(".product-title > a")
         file.write(title[0].text + "\n")
 
+# page number
 page = 2
 
+# collecting data from other pages
 with open("new.txt", "a", encoding="utf-8")as file:
     while True:
         response = requests.get(
